a64ce1b7df90798e166a09aa7aae7c60216c6a7c
nestor_leiva <nestorjoel2@gmail.com>
2024-02-21 21:18:15 -0600

    feat: atributos

---
+++ Proveeduria/BLL/Proveedor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Proveedor : Persona
+    {
+        public Proveedor() { }
+
+        public Proveedor(Guid codigoProveedor)
+        {
+            this.codigoProveedor = codigoProveedor;
+        }
+
+        private Guid codigoProveedor = Guid.Empty;
+        
+
+
+
+    }
+}
