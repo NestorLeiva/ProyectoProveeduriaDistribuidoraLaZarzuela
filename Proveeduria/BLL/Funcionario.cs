1b135da9c41d08695e04d8bcf79657e1be5d8ee1
nestor_leiva <nestorjoel2@gmail.com>
2024-02-22 14:02:57 -0600

    feat: metodo grabarXML Funcionario

--- Proveeduria/BLL/Funcionario.cs
+++ Proveeduria/BLL/Funcionario.cs
@@ -3,28 +3,128 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Xml;
+using System.IO;
 
 namespace BLL
 {
     public class Funcionario : Persona /*herencia de clase Persona*/
     {
-        public Funcionario() { }
+        public Funcionario() { } /*Constructor Vacio*/
 
-        public Funcionario(string contrasenia, Guid codigoFuncionario)
+        /*------------------------------------------------- Objetos --------------------------------------------------------------------*/
+        XmlDocument xmlDocFuncionario = new XmlDocument();
+
+        DAL.ArchivoXML _ArchivoXML = new DAL.ArchivoXML(); /*instancia de la clase ArchivoXML en la Libreria DAL*/
+
+        public Funcionario(string contrasenia, string estado)
         {
             Contrasenia = contrasenia;
-            CodigoFuncionario = codigoFuncionario;
- 
+            Estado = estado;
         }
 
-        private string contrasenia = string.Empty;  
-        private Guid codigoFuncionario = Guid.Empty;
+        /*------------------------------------------------- Atributos --------------------------------------------------------------------*/
 
+        private string contrasenia = string.Empty;
+        private int codFuncionario;
+        private string estado = string.Empty;
 
         /*Getter & Setter*/
-        public string Contrasenia { get { return contrasenia; } set {  contrasenia = value; } }
-        public Guid CodigoFuncionario { get {  return codigoFuncionario; } set {  codigoFuncionario = value; } }
+        public string Contrasenia { get { return contrasenia; } set { contrasenia = value; } }
+        public int CodFuncionario { get { return codFuncionario; } set { codFuncionario = value; } }
+        public string Estado { get { return estado; } set { estado = value; } }
+
+        /*------------------------------------------------- Metodos --------------------------------------------------------------------*/
+
+        public string generarEmail()
+        {
+            return $"{Nombre.Substring(0, 1)}{ApellidoPrimero}{ApellidoSegundo.Substring(0, 1)}@LAZARZUELA.COM";
+            /*metodo para generar automaticamente el correo del funcionario 
+             1 letra del nombre + apellidoPrimero + 1 letra del apellidoSegundo + dominio '@LAZARZUELA.COM'*/
+        } /*fin generarEmail*/
+
+        public void grabarXMLFuncionario(string rutaArchivo)
+        {
+            /*Validacion si existe el archivo si existe lo lee. Si no existe lo crea*/
+            if (File.Exists(rutaArchivo))
+            {
+                xmlDocFuncionario.Load(rutaArchivo);
+                /*cargo el archivo*/
+
+                XmlNode existeFuncionario = xmlDocFuncionario.SelectSingleNode($"//Funcionario [Identificacion='{this.DNI}']");
+                /*valido si ya existe un usuario con la mista Identificicacion*/
+                if (existeFuncionario != null)
+                {
+                    throw new Exception("*** Ya se encuentra Registrado el Funcionario ***");
+                }
+
+            }
+            else
+            {
+                /*Nodo Funcionarios*/
+                XmlNode xmlRoot = xmlDocFuncionario.CreateElement("Funcionarios");
+                xmlDocFuncionario.AppendChild(xmlRoot);
+            }
+
+            /*nodo Funcionario*/
+            XmlNode xmlFuncionario = xmlDocFuncionario.CreateElement("Funcionario");
+            xmlDocFuncionario.DocumentElement.AppendChild(xmlFuncionario);
+            /*accedo al nodo padre por medio del DocumentElement y posterior agrego los nodos Hijos */
+
+            /*Nodo DNI*/
+            XmlNode xmlDNI = xmlDocFuncionario.CreateElement("Identificacion");
+            xmlDNI.InnerText = this.DNI.ToString();
+            xmlFuncionario.AppendChild(xmlDNI);
+
+            /*Nodo Nombre*/
+            XmlNode xmlNombre = xmlDocFuncionario.CreateElement("Nombre");
+            xmlNombre.InnerText = this.Nombre;
+            xmlFuncionario.AppendChild(xmlNombre);
+
+            /*Nodo ApellidoPrimero*/
+            XmlNode xmlApellidoPrimero = xmlDocFuncionario.CreateElement("ApellidoPrimero");
+            xmlApellidoPrimero.InnerText = this.ApellidoPrimero;
+            xmlFuncionario.AppendChild(xmlApellidoPrimero);
+
+            /*Nodo ApellidoSegundo*/
+            XmlNode xmlApellidoSegundo = xmlDocFuncionario.CreateElement("ApellidoSegundo");
+            xmlApellidoSegundo.InnerText = this.ApellidoSegundo;
+            xmlFuncionario.AppendChild(xmlApellidoSegundo);
+
+            /*Nodo Email*/
+            XmlNode xmlEmail = xmlDocFuncionario.CreateElement("Email");
+            xmlEmail.InnerText = this.generarEmail(); /*invoco al metodo*/
+            xmlFuncionario.AppendChild(xmlEmail);
 
 
-    }
-}
+            /*Nodo CodigoFuncionario*/
+            XmlNode xmlCodFuncionario = xmlDocFuncionario.CreateElement("CodigoFuncionario");
+            xmlCodFuncionario.InnerText = this.DNI.ToString();
+            xmlFuncionario.AppendChild(xmlCodFuncionario);
+
+            /*Nodo Contrasenia*/
+            XmlNode xmlContrasenia = xmlDocFuncionario.CreateElement("Contrasenia");
+            xmlContrasenia.InnerText = this.Contrasenia;
+            xmlFuncionario.AppendChild(xmlContrasenia);
+
+            /*Nodo Estado*/
+            XmlNode xmlEstado = xmlDocFuncionario.CreateElement("Estado");
+            xmlEstado.InnerText = this.Estado;
+            xmlFuncionario.AppendChild(xmlEstado);
+
+
+            _ArchivoXML.escribirXML(rutaArchivo, xmlDocFuncionario);
+            /*ingreso a la instancia para realizar la escritura del XML*/
+
+        } /*fin metodo GrabarFuncionario*/
+
+
+
+
+
+
+
+
+    }/*fin clase Funcionario*/
+}/*fin namespace*/
