9d92b9a4e7211dcbb08fe9dc0765347dee200521
nestor_leiva <nestorjoel2@gmail.com>
2024-02-25 21:26:40 -0600

    feat: creacion de clase

---
+++ Proveeduria/BLL/IngresoFacturas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class IngresoFacturas : Productos
+    {
+        /*------------------------------------------------- Atributos --------------------------------------------------------------------*/
+
+        private string codigoProv = string.Empty;
+        private string nombreProv = string.Empty;
+        private DateTime fechaFactura;
+        private string numeroFactura = string.Empty;
+        private int montoFactura;
+        private double montoIVA;
+
+
+        public List<Productos> lstProductos { get; set; } = new List<Productos>(); 
+
+        /*------------------------------------------------- Getter & Setter --------------------------------------------------------------------*/
+
+        public string CodigoProv { get { return codigoProv; } set { codigoProv = value; } }
+        public string NombreProv { get { return nombreProv; } set { nombreProv = value; } }
+        public DateTime FechaFactura { get { return fechaFactura; } set { fechaFactura = value; } }
+        public string NumeroFactura { get { return numeroFactura; } set { numeroFactura = value; } }
+        public int MontoFactura { get { return montoFactura; } set { montoFactura = value; } }
+        public double MontoIVA { get { return montoIVA; } set { montoIVA = value; } }
+        
+
+
+        /*------------------------------------------------- Metodos --------------------------------------------------------------------*/
+
+
+
+
+
+
+    }/*fin IngresoFacturas*/
+}/*fin namespace*/
---
+++ Proveeduria/BLL/Productos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Productos
+    {
+        /*------------------------------------------------- Atributos --------------------------------------------------------------------*/
+        private string categoriaProducto = string.Empty;
+        private string desProducto = string.Empty;
+        private int cantProducto;
+        private double precioProducto;
+
+        /*------------------------------------------------- Getter & Setter --------------------------------------------------------------------*/
+        public string CategoriaProducto { get { return categoriaProducto; } set { categoriaProducto = value; } }
+        public string DesProducto { get { return desProducto; } set { desProducto = value} }
+        public int CantProducto { get { return cantProducto; } set { cantProducto = value; } }
+        public double PrecioProducto { get { return precioProducto; } set { precioProducto = value; } }
+
+        /*------------------------------------------------- Metodos --------------------------------------------------------------------*/
+
+
+    }/*Fin Productos*/
+}/*fin namespace*/
