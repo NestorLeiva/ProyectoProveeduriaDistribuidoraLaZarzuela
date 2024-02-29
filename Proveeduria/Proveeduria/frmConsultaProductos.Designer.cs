f59335973f4af8f64872dd978cf154726b2f3632
nestor_leiva <nestorjoel2@gmail.com>
2024-02-28 22:59:51 -0600

    feat: validaciones Texbox, btnBuscar, metodo CargarListaProductos

--- Proveeduria/Proveeduria/frmConsultaProductos.Designer.cs
+++ Proveeduria/Proveeduria/frmConsultaProductos.Designer.cs
@@ -28,19 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            gbxConsultaProductos = new GroupBox();
+            btnLimpiar = new Button();
+            btnBuscarCodigo = new Button();
+            txtBuscarCodigo = new TextBox();
+            lblBuscarCodigo = new Label();
+            gbxListaProductos = new GroupBox();
+            lvwListaProductos = new ListView();
+            colCatProducto = new ColumnHeader();
+            colCodProducto = new ColumnHeader();
+            colDesProducto = new ColumnHeader();
+            colCantidadProducto = new ColumnHeader();
+            colPrecioUndProducto = new ColumnHeader();
+            gbxConsultaProductos.SuspendLayout();
+            gbxListaProductos.SuspendLayout();
             SuspendLayout();
+            // 
+            // gbxConsultaProductos
+            // 
+            gbxConsultaProductos.Controls.Add(btnLimpiar);
+            gbxConsultaProductos.Controls.Add(btnBuscarCodigo);
+            gbxConsultaProductos.Controls.Add(txtBuscarCodigo);
+            gbxConsultaProductos.Controls.Add(lblBuscarCodigo);
+            gbxConsultaProductos.Dock = DockStyle.Top;
+            gbxConsultaProductos.Location = new Point(0, 0);
+            gbxConsultaProductos.Name = "gbxConsultaProductos";
+            gbxConsultaProductos.Size = new Size(800, 64);
+            gbxConsultaProductos.TabIndex = 1;
+            gbxConsultaProductos.TabStop = false;
+            gbxConsultaProductos.Text = "Consultar por nombre";
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLimpiar.Location = new Point(444, 22);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(90, 28);
+            btnLimpiar.TabIndex = 4;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // btnBuscarCodigo
+            // 
+            btnBuscarCodigo.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnBuscarCodigo.Location = new Point(348, 22);
+            btnBuscarCodigo.Name = "btnBuscarCodigo";
+            btnBuscarCodigo.Size = new Size(90, 28);
+            btnBuscarCodigo.TabIndex = 3;
+            btnBuscarCodigo.Text = "Buscar";
+            btnBuscarCodigo.UseVisualStyleBackColor = true;
+            btnBuscarCodigo.Click += btnBuscarCodigo_Click;
+            // 
+            // txtBuscarCodigo
+            // 
+            txtBuscarCodigo.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtBuscarCodigo.Location = new Point(141, 25);
+            txtBuscarCodigo.Name = "txtBuscarCodigo";
+            txtBuscarCodigo.Size = new Size(201, 24);
+            txtBuscarCodigo.TabIndex = 2;
+            txtBuscarCodigo.KeyPress += txtBuscarCodigo_KeyPress;
+            // 
+            // lblBuscarCodigo
+            // 
+            lblBuscarCodigo.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblBuscarCodigo.Location = new Point(12, 19);
+            lblBuscarCodigo.Name = "lblBuscarCodigo";
+            lblBuscarCodigo.Size = new Size(123, 31);
+            lblBuscarCodigo.TabIndex = 1;
+            lblBuscarCodigo.Text = "Identificacion ";
+            lblBuscarCodigo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // gbxListaProductos
+            // 
+            gbxListaProductos.Controls.Add(lvwListaProductos);
+            gbxListaProductos.Dock = DockStyle.Fill;
+            gbxListaProductos.Location = new Point(0, 64);
+            gbxListaProductos.Name = "gbxListaProductos";
+            gbxListaProductos.Size = new Size(800, 386);
+            gbxListaProductos.TabIndex = 2;
+            gbxListaProductos.TabStop = false;
+            gbxListaProductos.Text = "Lista De Clientes";
+            // 
+            // lvwListaProductos
+            // 
+            lvwListaProductos.BackColor = SystemColors.Info;
+            lvwListaProductos.Columns.AddRange(new ColumnHeader[] { colCatProducto, colCodProducto, colDesProducto, colCantidadProducto, colPrecioUndProducto });
+            lvwListaProductos.Dock = DockStyle.Fill;
+            lvwListaProductos.Location = new Point(3, 19);
+            lvwListaProductos.Name = "lvwListaProductos";
+            lvwListaProductos.Size = new Size(794, 364);
+            lvwListaProductos.TabIndex = 42;
+            lvwListaProductos.UseCompatibleStateImageBehavior = false;
+            lvwListaProductos.View = View.Details;
+            // 
+            // colCatProducto
+            // 
+            colCatProducto.Text = "Categoria Producto";
+            colCatProducto.Width = 130;
+            // 
+            // colCodProducto
+            // 
+            colCodProducto.Text = "Codigo Producto";
+            colCodProducto.Width = 120;
+            // 
+            // colDesProducto
+            // 
+            colDesProducto.Text = "Descripcion Producto";
+            colDesProducto.Width = 130;
+            // 
+            // colCantidadProducto
+            // 
+            colCantidadProducto.Text = "Cantidad Producto";
+            colCantidadProducto.Width = 120;
+            // 
+            // colPrecioUndProducto
+            // 
+            colPrecioUndProducto.Text = "Precio Unitario";
+            colPrecioUndProducto.Width = 120;
             // 
             // frmConsultaProductos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            ControlBox = false;
+            Controls.Add(gbxListaProductos);
+            Controls.Add(gbxConsultaProductos);
             Name = "frmConsultaProductos";
-            Text = "frmConsultaProductos";
+            Text = "Consulta Productos";
+            Load += frmConsultaProductos_Load;
+            gbxConsultaProductos.ResumeLayout(false);
+            gbxConsultaProductos.PerformLayout();
+            gbxListaProductos.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox gbxConsultaProductos;
+        private Button btnLimpiar;
+        private Button btnBuscarCodigo;
+        private TextBox txtBuscarCodigo;
+        private Label lblBuscarCodigo;
+        private GroupBox gbxListaProductos;
+        private ListView lvwListaProductos;
+        private ColumnHeader colCatProducto;
+        private ColumnHeader colCodProducto;
+        private ColumnHeader colDesProducto;
+        private ColumnHeader colCantidadProducto;
+        private ColumnHeader colPrecioUndProducto;
     }
 }
--- Proveeduria/Proveeduria/frmConsultaProductos.cs
+++ Proveeduria/Proveeduria/frmConsultaProductos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using Utils;
 
 namespace Proveeduria
 {
@@ -16,5 +19,99 @@ namespace Proveeduria
         {
             InitializeComponent();
         }
-    }
-}
+
+        private void frmConsultaProductos_Load(object sender, EventArgs e)
+        {
+            cargarListaProductos("ListaProductos.xml");
+        }
+
+        /*------------------------------------------------- Objetos --------------------------------------------------------------------*/
+
+        DAL.ArchivoXML _ArchivoXML = new DAL.ArchivoXML();
+        private string conusltaProducto; /*btn Buscar Producto*/
+
+        /*------------------------------------------------- TextBox --------------------------------------------------------------------*/
+        private void txtBuscarCodigo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Validaciones.soloLetras(e.KeyChar.ToString()) && !Validaciones.soloNumeros(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /*------------------------------------------------- Botones --------------------------------------------------------------------*/
+        private void btnBuscarCodigo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conusltaProducto = txtBuscarCodigo.Text.ToUpper(); /*obtengo el DNI a consultar*/
+
+                lvwListaProductos.Items.Clear();/*limieza del listview*/
+                XmlDocument xmlDoc = _ArchivoXML.leerXML("ListaProductos.xml"); /*lectura del XML*/
+
+                XmlNode _nodoCliente = xmlDoc.SelectSingleNode($"//Producto[CodigoProducto='{conusltaProducto}']");
+
+                /* se realiza la busqueda en todos los nodos "Hijo /
+				  [NumeroIdentificacion='{consultaDNI}'] es una exprecion de busqueda en los atributos Id con un valor igual al de la variable consultaDNI  "*/
+
+                if (_nodoCliente != null)
+                {
+                    ListViewItem _itemDNI = new ListViewItem(_nodoCliente.SelectSingleNode("CategoriaProducto").InnerText);/*se realiza la busqueda en el XML*/
+
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("CodigoProducto").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("NombreProducto").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("CantidadProducto").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("PrecioProducto").InnerText);
+                    /*Obtengo los datos */
+
+                    lvwListaProductos.Items.Add(_itemDNI); /*Agrego los datos al listview*/
+                }
+                else
+                {
+                    MessageBox.Show("No se encotro al Producto con el ID: " + conusltaProducto, "Distribuidora La Zarzuela", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Distribuidora La Zarzuela", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtBuscarCodigo.Text = string.Empty;
+            cargarListaProductos("ListaProductos.xml");
+        }
+        /*------------------------------------------------- Metodos --------------------------------------------------------------------*/
+
+        public void cargarListaProductos(string rutaArchivo)
+        {
+            lvwListaProductos.Items.Clear();
+
+            XmlDocument xmlDoc = _ArchivoXML.leerXML(rutaArchivo); /*realizo la lectura del archivo*/
+
+            XmlNodeList _Productos = xmlDoc.SelectNodes("//Producto");   /*obtengo los nodos del XML*/
+
+            foreach (XmlNode cliente in _Productos)
+            {
+                string CategoriaProducto = cliente.SelectSingleNode("CategoriaProducto").InnerText;
+                string NombreProducto = cliente.SelectSingleNode("NombreProducto").InnerText;
+                string CodigoProducto = cliente.SelectSingleNode("CodigoProducto").InnerText;
+                string CantidadProducto = cliente.SelectSingleNode("CantidadProducto").InnerText;
+                string PrecioProducto = cliente.SelectSingleNode("PrecioProducto").InnerText;
+                
+                /*obtengo los datos del XML y los almacelo en las variables*/
+
+                ListViewItem itemCliente = new ListViewItem(
+                    new[] { CategoriaProducto, NombreProducto,CodigoProducto,CantidadProducto,PrecioProducto });
+                /*pinto los datos en el ListView */
+
+                lvwListaProductos.Items.Add(itemCliente);
+
+            }/*iteracion por los nodos */
+
+        }/*fin cargarLista Productos*/
+
+
+    }/*fin frmConsultaProductos*/
+}/*fin namespace*/
