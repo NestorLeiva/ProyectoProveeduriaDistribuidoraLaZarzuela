2fb373beae67b8ebb5a6c7a53bf76acdca32d20c
nestor_leiva <nestorjoel2@gmail.com>
2024-02-28 22:15:26 -0600

    feat(front-end): cargar Lista Clientes & btnBuscarCliente

--- Proveeduria/Proveeduria/frmConsultaClientes.Designer.cs
+++ Proveeduria/Proveeduria/frmConsultaClientes.Designer.cs
@@ -28,19 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            gbxConsultaClientes = new GroupBox();
+            btnBuscarCliente = new Button();
+            txtBuscarIdentificacion = new TextBox();
+            label1 = new Label();
+            gbxListaClientes = new GroupBox();
+            lvwListaClientes = new ListView();
+            colNumIdentificacion = new ColumnHeader();
+            colTipoIDentificacion = new ColumnHeader();
+            colCodigoCliente = new ColumnHeader();
+            colNombre = new ColumnHeader();
+            colApellidoPrimero = new ColumnHeader();
+            colApellidoSegundo = new ColumnHeader();
+            colTelefono = new ColumnHeader();
+            colEmail = new ColumnHeader();
+            colProvincia = new ColumnHeader();
+            colCanton = new ColumnHeader();
+            colDistrito = new ColumnHeader();
+            colOtrasSenias = new ColumnHeader();
+            btnLimpiar = new Button();
+            gbxConsultaClientes.SuspendLayout();
+            gbxListaClientes.SuspendLayout();
             SuspendLayout();
+            // 
+            // gbxConsultaClientes
+            // 
+            gbxConsultaClientes.Controls.Add(btnLimpiar);
+            gbxConsultaClientes.Controls.Add(btnBuscarCliente);
+            gbxConsultaClientes.Controls.Add(txtBuscarIdentificacion);
+            gbxConsultaClientes.Controls.Add(label1);
+            gbxConsultaClientes.Dock = DockStyle.Top;
+            gbxConsultaClientes.Location = new Point(0, 0);
+            gbxConsultaClientes.Name = "gbxConsultaClientes";
+            gbxConsultaClientes.Size = new Size(800, 64);
+            gbxConsultaClientes.TabIndex = 0;
+            gbxConsultaClientes.TabStop = false;
+            gbxConsultaClientes.Text = "Buscar por Numero de Identificacion de Cliente";
+            // 
+            // btnBuscarCliente
+            // 
+            btnBuscarCliente.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnBuscarCliente.Location = new Point(348, 22);
+            btnBuscarCliente.Name = "btnBuscarCliente";
+            btnBuscarCliente.Size = new Size(90, 28);
+            btnBuscarCliente.TabIndex = 3;
+            btnBuscarCliente.Text = "Buscar";
+            btnBuscarCliente.UseVisualStyleBackColor = true;
+            btnBuscarCliente.Click += btnBuscarCliente_Click;
+            // 
+            // txtBuscarIdentificacion
+            // 
+            txtBuscarIdentificacion.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtBuscarIdentificacion.Location = new Point(141, 25);
+            txtBuscarIdentificacion.Name = "txtBuscarIdentificacion";
+            txtBuscarIdentificacion.Size = new Size(201, 24);
+            txtBuscarIdentificacion.TabIndex = 2;
+            txtBuscarIdentificacion.KeyPress += txtBuscarIdentificacion_KeyPress;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 31);
+            label1.TabIndex = 1;
+            label1.Text = "Identificacion ";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // gbxListaClientes
+            // 
+            gbxListaClientes.Controls.Add(lvwListaClientes);
+            gbxListaClientes.Dock = DockStyle.Fill;
+            gbxListaClientes.Location = new Point(0, 64);
+            gbxListaClientes.Name = "gbxListaClientes";
+            gbxListaClientes.Size = new Size(800, 386);
+            gbxListaClientes.TabIndex = 5;
+            gbxListaClientes.TabStop = false;
+            gbxListaClientes.Text = "Lista De Clientes";
+            // 
+            // lvwListaClientes
+            // 
+            lvwListaClientes.BackColor = SystemColors.Info;
+            lvwListaClientes.Columns.AddRange(new ColumnHeader[] { colNumIdentificacion, colTipoIDentificacion, colCodigoCliente, colNombre, colApellidoPrimero, colApellidoSegundo, colTelefono, colEmail, colProvincia, colCanton, colDistrito, colOtrasSenias });
+            lvwListaClientes.Dock = DockStyle.Fill;
+            lvwListaClientes.Location = new Point(3, 19);
+            lvwListaClientes.Name = "lvwListaClientes";
+            lvwListaClientes.Size = new Size(794, 364);
+            lvwListaClientes.TabIndex = 6;
+            lvwListaClientes.UseCompatibleStateImageBehavior = false;
+            lvwListaClientes.View = View.Details;
+            // 
+            // colNumIdentificacion
+            // 
+            colNumIdentificacion.Text = "Numero Identificacion";
+            colNumIdentificacion.Width = 140;
+            // 
+            // colTipoIDentificacion
+            // 
+            colTipoIDentificacion.Text = "Tipo Identificacion";
+            colTipoIDentificacion.Width = 140;
+            // 
+            // colCodigoCliente
+            // 
+            colCodigoCliente.Text = "Codigo Cliente";
+            colCodigoCliente.Width = 120;
+            // 
+            // colNombre
+            // 
+            colNombre.Text = "Nombre";
+            colNombre.Width = 120;
+            // 
+            // colApellidoPrimero
+            // 
+            colApellidoPrimero.Text = "Primer Apellido";
+            colApellidoPrimero.Width = 120;
+            // 
+            // colApellidoSegundo
+            // 
+            colApellidoSegundo.Text = "Segundo Apellido";
+            colApellidoSegundo.Width = 120;
+            // 
+            // colTelefono
+            // 
+            colTelefono.Text = "Telefono";
+            colTelefono.Width = 120;
+            // 
+            // colEmail
+            // 
+            colEmail.Text = "Email";
+            colEmail.Width = 120;
+            // 
+            // colProvincia
+            // 
+            colProvincia.Text = "Provincia";
+            colProvincia.Width = 120;
+            // 
+            // colCanton
+            // 
+            colCanton.Text = "Canton";
+            colCanton.Width = 120;
+            // 
+            // colDistrito
+            // 
+            colDistrito.Text = "Distrito";
+            colDistrito.Width = 120;
+            // 
+            // colOtrasSenias
+            // 
+            colOtrasSenias.Text = "Otras Senias";
+            colOtrasSenias.Width = 120;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLimpiar.Location = new Point(444, 22);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(90, 28);
+            btnLimpiar.TabIndex = 4;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
             // 
             // frmConsultaClientes
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            ControlBox = false;
+            Controls.Add(gbxListaClientes);
+            Controls.Add(gbxConsultaClientes);
             Name = "frmConsultaClientes";
-            Text = "frmConsultaClientes";
+            Text = "Consulta Clientes";
+            Load += frmConsultaClientes_Load;
+            gbxConsultaClientes.ResumeLayout(false);
+            gbxConsultaClientes.PerformLayout();
+            gbxListaClientes.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox gbxConsultaClientes;
+        private TextBox txtBuscarIdentificacion;
+        private Label label1;
+        private GroupBox gbxListaClientes;
+        private Button btnBuscarCliente;
+        private ListView lvwListaClientes;
+        private ColumnHeader colNumIdentificacion;
+        private ColumnHeader colTipoIDentificacion;
+        private ColumnHeader colCodigoCliente;
+        private ColumnHeader colNombre;
+        private ColumnHeader colApellidoPrimero;
+        private ColumnHeader colApellidoSegundo;
+        private ColumnHeader colTelefono;
+        private ColumnHeader colEmail;
+        private ColumnHeader colProvincia;
+        private ColumnHeader colCanton;
+        private ColumnHeader colDistrito;
+        private ColumnHeader colOtrasSenias;
+        private Button btnLimpiar;
     }
 }
--- Proveeduria/Proveeduria/frmConsultaClientes.cs
+++ Proveeduria/Proveeduria/frmConsultaClientes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using Utils;
 
 namespace Proveeduria
 {
@@ -16,5 +19,111 @@ namespace Proveeduria
         {
             InitializeComponent();
         }
-    }
-}
+
+        /* ----------------------------------------------- Objetos -----------------------------------------------*/
+        DAL.ArchivoXML _ArchivoXML = new DAL.ArchivoXML();
+
+        private string consultaDNI;
+        private void frmConsultaClientes_Load(object sender, EventArgs e)
+        {
+            cargarListaClientes("Clientes.xml");
+        }
+
+        /* ----------------------------------------------- TextBox -----------------------------------------------*/
+        private void txtBuscarIdentificacion_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Validaciones.soloNumeros(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /* ----------------------------------------------- Botones -----------------------------------------------*/
+        private void btnBuscarCliente_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                consultaDNI = txtBuscarIdentificacion.Text; /*obtengo el DNI a consultar*/
+
+                lvwListaClientes.Items.Clear();/*limieza del listview*/
+                XmlDocument xmlDoc = _ArchivoXML.leerXML("Clientes.xml"); /*lectura del XML*/
+
+                XmlNode _nodoCliente = xmlDoc.SelectSingleNode($"//Cliente[NumeroIdentificacion='{consultaDNI}']");
+                
+                /* se realiza la busqueda en todos los nodos "Hijo /
+				  [NumeroIdentificacion='{consultaDNI}'] es una exprecion de busqueda en los atributos Id con un valor igual al de la variable consultaDNI  "*/
+
+                if (_nodoCliente != null)
+                {
+                    ListViewItem _itemDNI = new ListViewItem(_nodoCliente.SelectSingleNode("NumeroIdentificacion").InnerText);/*se realiza la busqueda en el XML*/
+                    ListViewItem _itemCodigo = new ListViewItem(_nodoCliente.SelectSingleNode("CodigoCliente").InnerText);/*se realiza la busqueda en el XML*/
+
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("TipoIdentificacion").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("Nombre").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("ApellidoPrimero").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("ApellidoSegundo").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("CodigoCliente").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("Telefono").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("Email").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("Provincia").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("Canton").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("Distrito").InnerText);
+                    _itemDNI.SubItems.Add(_nodoCliente.SelectSingleNode("OtrasSenias").InnerText);
+                    
+                    lvwListaClientes.Items.Add(_itemDNI); /*Agrego los datos al listview*/
+                   
+                }
+                else
+                {
+                    MessageBox.Show("No se encotro al cliente con el ID: " + consultaDNI, "Distribuidora La Zarzuela", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Distribuidora La Zarzuela", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtBuscarIdentificacion.Text = string.Empty;
+            cargarListaClientes("Clientes.xml");
+        }
+
+        /* ----------------------------------------------- Metodos -----------------------------------------------*/
+        public void cargarListaClientes(string rutaArchivo)
+        {
+            lvwListaClientes.Items.Clear();
+
+            XmlDocument xmlDoc = _ArchivoXML.leerXML(rutaArchivo); /*realizo la lectura del archivo*/
+
+            XmlNodeList _Clientes = xmlDoc.SelectNodes("//Cliente");   /*obtengo los nodos del XML*/
+
+            foreach (XmlNode cliente in _Clientes)
+            {
+                string DNI = cliente.SelectSingleNode("NumeroIdentificacion").InnerText;
+                string TipoDNI = cliente.SelectSingleNode("TipoIdentificacion").InnerText;
+                string Nombre = cliente.SelectSingleNode("Nombre").InnerText;
+                string ApellidoPrimero = cliente.SelectSingleNode("ApellidoPrimero").InnerText;
+                string ApellidoSegundo = cliente.SelectSingleNode("ApellidoSegundo").InnerText;
+                string CodigoCliente = cliente.SelectSingleNode("CodigoCliente").InnerText;
+                string Telefono = cliente.SelectSingleNode("Telefono").InnerText;
+                string Email = cliente.SelectSingleNode("Email").InnerText;
+                string Provincia = cliente.SelectSingleNode("Provincia").InnerText;
+                string Canton = cliente.SelectSingleNode("Canton").InnerText;
+                string Distrito = cliente.SelectSingleNode("Distrito").InnerText;
+                string OtrasSenias = cliente.SelectSingleNode("OtrasSenias").InnerText;
+                /*obtengo los datos del XML y los almacelo en las variables*/
+
+                ListViewItem itemCliente = new ListViewItem(
+                    new[] { DNI, TipoDNI, CodigoCliente, Nombre, ApellidoPrimero, ApellidoSegundo, Telefono, Email, Provincia, Canton, Distrito, OtrasSenias });
+                /*pinto los datos en el ListView */
+
+                lvwListaClientes.Items.Add(itemCliente);
+
+            }/*iteracion por los nodos */
+
+        }/*fin cargarListaClientes*/
+
+
+    }/*fin frmConsultaClientes*/
+}/*fin namespace*/
