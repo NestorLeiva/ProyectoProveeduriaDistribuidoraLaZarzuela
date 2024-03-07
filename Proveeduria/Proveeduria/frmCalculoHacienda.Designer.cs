0e779c363083e2f4ada9e2eda49e5ed1386befb4
nestor_leiva <nestorjoel2@gmail.com>
2024-03-06 22:46:31 -0600

    feat(metodos): Calcular Impuesto y BuscarFacturas

--- Proveeduria/Proveeduria/frmCalculoHacienda.Designer.cs
+++ Proveeduria/Proveeduria/frmCalculoHacienda.Designer.cs
@@ -30,23 +30,37 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculoHacienda));
             gbxRegistroProductosTitulo = new GroupBox();
+            gbxConsultarFecha = new GroupBox();
+            btnBuscar = new Button();
+            dtpFechaConsulta = new DateTimePicker();
             pbLogo = new PictureBox();
             lblImpuestosTitulo = new Label();
             gbxCalculoIVA = new GroupBox();
+            btnSalir = new Button();
+            btnCalcular = new Button();
+            lblCalculoRes = new Label();
             gbxListView = new GroupBox();
-            gbxFacturasCompra = new GroupBox();
             gbxFacturasVenta = new GroupBox();
-            lvwFacturasCompra = new ListView();
             lvwFacturasVenta = new ListView();
-            dtpFechaConsulta = new DateTimePicker();
-            gbxConsultarFecha = new GroupBox();
-            btnBuscar = new Button();
+            colClienteVenta = new ColumnHeader();
+            colFacturaVenta = new ColumnHeader();
+            colFechaVenta = new ColumnHeader();
+            colMontoIVAVenta = new ColumnHeader();
+            colTotalVenta = new ColumnHeader();
+            gbxFacturasCompra = new GroupBox();
+            lvwFacturasCompra = new ListView();
+            colProveedorCompra = new ColumnHeader();
+            colFacturaCompra = new ColumnHeader();
+            colFechaCompra = new ColumnHeader();
+            colMontoIVACompra = new ColumnHeader();
+            ColTotalCompra = new ColumnHeader();
             gbxRegistroProductosTitulo.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pbLogo).BeginInit();
-            gbxListView.SuspendLayout();
-            gbxFacturasCompra.SuspendLayout();
-            gbxFacturasVenta.SuspendLayout();
             gbxConsultarFecha.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pbLogo).BeginInit();
+            gbxCalculoIVA.SuspendLayout();
+            gbxListView.SuspendLayout();
+            gbxFacturasVenta.SuspendLayout();
+            gbxFacturasCompra.SuspendLayout();
             SuspendLayout();
             // 
             // gbxRegistroProductosTitulo
@@ -57,109 +71,15 @@
             gbxRegistroProductosTitulo.Dock = DockStyle.Top;
             gbxRegistroProductosTitulo.Location = new Point(0, 0);
             gbxRegistroProductosTitulo.Name = "gbxRegistroProductosTitulo";
-            gbxRegistroProductosTitulo.Size = new Size(884, 116);
+            gbxRegistroProductosTitulo.Size = new Size(889, 104);
             gbxRegistroProductosTitulo.TabIndex = 2;
             gbxRegistroProductosTitulo.TabStop = false;
-            // 
-            // pbLogo
-            // 
-            pbLogo.Image = (Image)resources.GetObject("pbLogo.Image");
-            pbLogo.Location = new Point(12, 19);
-            pbLogo.Name = "pbLogo";
-            pbLogo.Size = new Size(126, 86);
-            pbLogo.SizeMode = PictureBoxSizeMode.StretchImage;
-            pbLogo.TabIndex = 14;
-            pbLogo.TabStop = false;
-            // 
-            // lblImpuestosTitulo
-            // 
-            lblImpuestosTitulo.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            lblImpuestosTitulo.Location = new Point(262, 19);
-            lblImpuestosTitulo.Name = "lblImpuestosTitulo";
-            lblImpuestosTitulo.Size = new Size(224, 86);
-            lblImpuestosTitulo.TabIndex = 16;
-            lblImpuestosTitulo.Text = "Calculo de Impuestos ";
-            lblImpuestosTitulo.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // gbxCalculoIVA
-            // 
-            gbxCalculoIVA.Dock = DockStyle.Bottom;
-            gbxCalculoIVA.Location = new Point(0, 661);
-            gbxCalculoIVA.Name = "gbxCalculoIVA";
-            gbxCalculoIVA.Size = new Size(884, 100);
-            gbxCalculoIVA.TabIndex = 3;
-            gbxCalculoIVA.TabStop = false;
-            gbxCalculoIVA.Text = "Calculo IVA";
-            // 
-            // gbxListView
-            // 
-            gbxListView.Controls.Add(gbxFacturasVenta);
-            gbxListView.Controls.Add(gbxFacturasCompra);
-            gbxListView.Dock = DockStyle.Fill;
-            gbxListView.Location = new Point(0, 116);
-            gbxListView.Name = "gbxListView";
-            gbxListView.Size = new Size(884, 545);
-            gbxListView.TabIndex = 4;
-            gbxListView.TabStop = false;
-            // 
-            // gbxFacturasCompra
-            // 
-            gbxFacturasCompra.Controls.Add(lvwFacturasCompra);
-            gbxFacturasCompra.Dock = DockStyle.Left;
-            gbxFacturasCompra.Location = new Point(3, 19);
-            gbxFacturasCompra.Name = "gbxFacturasCompra";
-            gbxFacturasCompra.Size = new Size(423, 523);
-            gbxFacturasCompra.TabIndex = 0;
-            gbxFacturasCompra.TabStop = false;
-            gbxFacturasCompra.Text = "Facturas Compra";
-            // 
-            // gbxFacturasVenta
-            // 
-            gbxFacturasVenta.Controls.Add(lvwFacturasVenta);
-            gbxFacturasVenta.Dock = DockStyle.Right;
-            gbxFacturasVenta.Location = new Point(432, 19);
-            gbxFacturasVenta.Name = "gbxFacturasVenta";
-            gbxFacturasVenta.Size = new Size(449, 523);
-            gbxFacturasVenta.TabIndex = 1;
-            gbxFacturasVenta.TabStop = false;
-            gbxFacturasVenta.Text = "Facturas Venta";
-            // 
-            // lvwFacturasCompra
-            // 
-            lvwFacturasCompra.BackColor = SystemColors.Info;
-            lvwFacturasCompra.Dock = DockStyle.Fill;
-            lvwFacturasCompra.Location = new Point(3, 19);
-            lvwFacturasCompra.Name = "lvwFacturasCompra";
-            lvwFacturasCompra.Size = new Size(417, 501);
-            lvwFacturasCompra.TabIndex = 0;
-            lvwFacturasCompra.UseCompatibleStateImageBehavior = false;
-            lvwFacturasCompra.View = View.Details;
-            // 
-            // lvwFacturasVenta
-            // 
-            lvwFacturasVenta.BackColor = SystemColors.Info;
-            lvwFacturasVenta.Dock = DockStyle.Fill;
-            lvwFacturasVenta.Location = new Point(3, 19);
-            lvwFacturasVenta.Name = "lvwFacturasVenta";
-            lvwFacturasVenta.Size = new Size(443, 501);
-            lvwFacturasVenta.TabIndex = 0;
-            lvwFacturasVenta.UseCompatibleStateImageBehavior = false;
-            lvwFacturasVenta.View = View.Details;
-            // 
-            // dtpFechaConsulta
-            // 
-            dtpFechaConsulta.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            dtpFechaConsulta.Format = DateTimePickerFormat.Short;
-            dtpFechaConsulta.Location = new Point(6, 24);
-            dtpFechaConsulta.Name = "dtpFechaConsulta";
-            dtpFechaConsulta.Size = new Size(141, 25);
-            dtpFechaConsulta.TabIndex = 17;
             // 
             // gbxConsultarFecha
             // 
             gbxConsultarFecha.Controls.Add(btnBuscar);
             gbxConsultarFecha.Controls.Add(dtpFechaConsulta);
-            gbxConsultarFecha.Location = new Point(606, 49);
+            gbxConsultarFecha.Location = new Point(609, 22);
             gbxConsultarFecha.Name = "gbxConsultarFecha";
             gbxConsultarFecha.Size = new Size(266, 61);
             gbxConsultarFecha.TabIndex = 18;
@@ -175,12 +95,192 @@
             btnBuscar.TabIndex = 18;
             btnBuscar.Text = "Consultar";
             btnBuscar.UseVisualStyleBackColor = true;
+            btnBuscar.Click += btnBuscar_Click;
+            // 
+            // dtpFechaConsulta
+            // 
+            dtpFechaConsulta.Format = DateTimePickerFormat.Short;
+            dtpFechaConsulta.Location = new Point(6, 26);
+            dtpFechaConsulta.Name = "dtpFechaConsulta";
+            dtpFechaConsulta.Size = new Size(141, 23);
+            dtpFechaConsulta.TabIndex = 19;
+            // 
+            // pbLogo
+            // 
+            pbLogo.Image = (Image)resources.GetObject("pbLogo.Image");
+            pbLogo.Location = new Point(12, 19);
+            pbLogo.Name = "pbLogo";
+            pbLogo.Size = new Size(119, 79);
+            pbLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbLogo.TabIndex = 14;
+            pbLogo.TabStop = false;
+            // 
+            // lblImpuestosTitulo
+            // 
+            lblImpuestosTitulo.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblImpuestosTitulo.Location = new Point(285, 22);
+            lblImpuestosTitulo.Name = "lblImpuestosTitulo";
+            lblImpuestosTitulo.Size = new Size(224, 61);
+            lblImpuestosTitulo.TabIndex = 16;
+            lblImpuestosTitulo.Text = "Calculo de Impuestos ";
+            lblImpuestosTitulo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // gbxCalculoIVA
+            // 
+            gbxCalculoIVA.Controls.Add(btnSalir);
+            gbxCalculoIVA.Controls.Add(btnCalcular);
+            gbxCalculoIVA.Controls.Add(lblCalculoRes);
+            gbxCalculoIVA.Dock = DockStyle.Bottom;
+            gbxCalculoIVA.Location = new Point(0, 713);
+            gbxCalculoIVA.Name = "gbxCalculoIVA";
+            gbxCalculoIVA.Size = new Size(889, 71);
+            gbxCalculoIVA.TabIndex = 3;
+            gbxCalculoIVA.TabStop = false;
+            gbxCalculoIVA.Text = "Calculo IVA";
+            // 
+            // btnSalir
+            // 
+            btnSalir.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSalir.Location = new Point(796, 31);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(81, 34);
+            btnSalir.TabIndex = 3;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCalcular.Location = new Point(102, 15);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(108, 41);
+            btnCalcular.TabIndex = 2;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // lblCalculoRes
+            // 
+            lblCalculoRes.BackColor = SystemColors.Info;
+            lblCalculoRes.BorderStyle = BorderStyle.FixedSingle;
+            lblCalculoRes.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblCalculoRes.Location = new Point(318, 13);
+            lblCalculoRes.Name = "lblCalculoRes";
+            lblCalculoRes.Size = new Size(322, 41);
+            lblCalculoRes.TabIndex = 1;
+            lblCalculoRes.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // gbxListView
+            // 
+            gbxListView.Controls.Add(gbxFacturasVenta);
+            gbxListView.Controls.Add(gbxFacturasCompra);
+            gbxListView.Dock = DockStyle.Fill;
+            gbxListView.Location = new Point(0, 104);
+            gbxListView.Name = "gbxListView";
+            gbxListView.Size = new Size(889, 609);
+            gbxListView.TabIndex = 4;
+            gbxListView.TabStop = false;
+            // 
+            // gbxFacturasVenta
+            // 
+            gbxFacturasVenta.Controls.Add(lvwFacturasVenta);
+            gbxFacturasVenta.Location = new Point(6, 302);
+            gbxFacturasVenta.Name = "gbxFacturasVenta";
+            gbxFacturasVenta.Size = new Size(869, 278);
+            gbxFacturasVenta.TabIndex = 1;
+            gbxFacturasVenta.TabStop = false;
+            gbxFacturasVenta.Text = "Facturas Venta";
+            // 
+            // lvwFacturasVenta
+            // 
+            lvwFacturasVenta.BackColor = SystemColors.Info;
+            lvwFacturasVenta.Columns.AddRange(new ColumnHeader[] { colClienteVenta, colFacturaVenta, colFechaVenta, colMontoIVAVenta, colTotalVenta });
+            lvwFacturasVenta.Dock = DockStyle.Fill;
+            lvwFacturasVenta.Location = new Point(3, 19);
+            lvwFacturasVenta.Name = "lvwFacturasVenta";
+            lvwFacturasVenta.Size = new Size(863, 256);
+            lvwFacturasVenta.TabIndex = 0;
+            lvwFacturasVenta.UseCompatibleStateImageBehavior = false;
+            lvwFacturasVenta.View = View.Details;
+            // 
+            // colClienteVenta
+            // 
+            colClienteVenta.Text = "Cliente";
+            colClienteVenta.Width = 100;
+            // 
+            // colFacturaVenta
+            // 
+            colFacturaVenta.Text = "Factura";
+            colFacturaVenta.Width = 100;
+            // 
+            // colFechaVenta
+            // 
+            colFechaVenta.Text = "Fecha";
+            colFechaVenta.Width = 100;
+            // 
+            // colMontoIVAVenta
+            // 
+            colMontoIVAVenta.Text = "Monto IVA";
+            colMontoIVAVenta.Width = 100;
+            // 
+            // colTotalVenta
+            // 
+            colTotalVenta.Text = "Total";
+            colTotalVenta.Width = 100;
+            // 
+            // gbxFacturasCompra
+            // 
+            gbxFacturasCompra.Controls.Add(lvwFacturasCompra);
+            gbxFacturasCompra.Location = new Point(3, 19);
+            gbxFacturasCompra.Name = "gbxFacturasCompra";
+            gbxFacturasCompra.Size = new Size(872, 280);
+            gbxFacturasCompra.TabIndex = 0;
+            gbxFacturasCompra.TabStop = false;
+            gbxFacturasCompra.Text = "Facturas Compra";
+            // 
+            // lvwFacturasCompra
+            // 
+            lvwFacturasCompra.BackColor = SystemColors.Info;
+            lvwFacturasCompra.Columns.AddRange(new ColumnHeader[] { colProveedorCompra, colFacturaCompra, colFechaCompra, colMontoIVACompra, ColTotalCompra });
+            lvwFacturasCompra.Dock = DockStyle.Fill;
+            lvwFacturasCompra.Location = new Point(3, 19);
+            lvwFacturasCompra.Name = "lvwFacturasCompra";
+            lvwFacturasCompra.Size = new Size(866, 258);
+            lvwFacturasCompra.TabIndex = 0;
+            lvwFacturasCompra.UseCompatibleStateImageBehavior = false;
+            lvwFacturasCompra.View = View.Details;
+            // 
+            // colProveedorCompra
+            // 
+            colProveedorCompra.Text = "Proveedor";
+            colProveedorCompra.Width = 100;
+            // 
+            // colFacturaCompra
+            // 
+            colFacturaCompra.Text = "Factura";
+            colFacturaCompra.Width = 100;
+            // 
+            // colFechaCompra
+            // 
+            colFechaCompra.Text = "Fecha";
+            colFechaCompra.Width = 100;
+            // 
+            // colMontoIVACompra
+            // 
+            colMontoIVACompra.Text = "Monto IVA";
+            colMontoIVACompra.Width = 100;
+            // 
+            // ColTotalCompra
+            // 
+            ColTotalCompra.Text = "Total";
+            ColTotalCompra.Width = 100;
             // 
             // frmCalculoHacienda
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(884, 761);
+            ClientSize = new Size(889, 784);
             ControlBox = false;
             Controls.Add(gbxListView);
             Controls.Add(gbxCalculoIVA);
@@ -190,11 +290,12 @@
             Text = "Calculo Impuestos";
             Load += frmCalculoHacienda_Load;
             gbxRegistroProductosTitulo.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pbLogo).EndInit();
-            gbxListView.ResumeLayout(false);
-            gbxFacturasCompra.ResumeLayout(false);
-            gbxFacturasVenta.ResumeLayout(false);
             gbxConsultarFecha.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pbLogo).EndInit();
+            gbxCalculoIVA.ResumeLayout(false);
+            gbxListView.ResumeLayout(false);
+            gbxFacturasVenta.ResumeLayout(false);
+            gbxFacturasCompra.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -212,5 +313,18 @@
         private GroupBox gbxConsultarFecha;
         private Button btnBuscar;
         private DateTimePicker dtpFechaConsulta;
+        private Button btnCalcular;
+        private Label lblCalculoRes;
+        private Button btnSalir;
+        private ColumnHeader colProveedorCompra;
+        private ColumnHeader colMontoIVACompra;
+        private ColumnHeader ColTotalCompra;
+        private ColumnHeader colFacturaCompra;
+        private ColumnHeader colFechaCompra;
+        private ColumnHeader colClienteVenta;
+        private ColumnHeader colFacturaVenta;
+        private ColumnHeader colFechaVenta;
+        private ColumnHeader colMontoIVAVenta;
+        private ColumnHeader colTotalVenta;
     }
 }
--- Proveeduria/Proveeduria/frmCalculoHacienda.cs
+++ Proveeduria/Proveeduria/frmCalculoHacienda.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using BLL;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Proveeduria
 {
@@ -17,13 +21,129 @@ namespace Proveeduria
             InitializeComponent();
         }
 
+
+        DAL.ArchivoXML _ArchivoXML = new DAL.ArchivoXML();
+        XmlDocument xmldDoc = new XmlDocument();
+
+        private DateTime fechaSeleccionada;
+        private DateTime fechaFactura;
+
+
         private void frmCalculoHacienda_Load(object sender, EventArgs e)
         {
 
         }
+        /*------------------------------------------------- Botones --------------------------------------------------------------------*/
+        private void btnSalir_Click(object sender, EventArgs e) { this.Close(); }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            BuscarFacturas("FacturasCompra", lvwFacturasCompra);
+            BuscarFacturas("FacturasVenta", lvwFacturasVenta);
+        } /*fin btn Buscar*/
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            CalcularImpuestoTotal();
+        }
+
+        /*------------------------------------------------- Metodos --------------------------------------------------------------------*/
+        public void BuscarFacturas(string tipoFactura, ListView listView)
+        {
+            try
+            {
+                listView.Items.Clear();
+                fechaSeleccionada = dtpFechaConsulta.Value;
 
 
+                // Asegurémonos de que el tipo de factura tenga un formato consistente
+                tipoFactura = tipoFactura.Replace(" ", ""); // Eliminar espacios en blanco
 
+                // Ruta relativa de la carpeta según la fecha seleccionada
+                string carpeta = fechaSeleccionada.ToString("yyyy-MM") + " " + tipoFactura;
+
+                // Ruta completa del directorio de facturas
+                string rutaCompleta = Path.Combine(Environment.CurrentDirectory, carpeta);
+
+
+                if (Directory.Exists(rutaCompleta))
+                {
+                    string[] archivosXML = Directory.GetFiles(rutaCompleta, "*.xml");
+                    /*Obtiene todos los archivos XML en la carpeta*/
+
+
+                    foreach (string _archivos in archivosXML)
+                    {
+                        xmldDoc = _ArchivoXML.leerXML(_archivos);
+                        XmlNode fechaNodo = xmldDoc.SelectSingleNode("//FechaFactura");
+
+                        int mesFactura = fechaNodo != null ? DateTime.Parse(fechaNodo.InnerText).Month : 0;
+                        int anioFactura = fechaNodo != null ? DateTime.Parse(fechaNodo.InnerText).Year : 0;
+                        /*Obtén el mes y año de la fecha de la factura*/
+
+                        if (mesFactura == fechaSeleccionada.Month && anioFactura == fechaSeleccionada.Year)
+                        {
+                            string numeroFactura = xmldDoc.SelectSingleNode("//NumeroFactura").InnerText;
+                            string nombreCliente = xmldDoc.SelectSingleNode("//Nombre").InnerText;
+                            string fechaFactura = xmldDoc.SelectSingleNode("//FechaFactura").InnerText;
+                            string montoIVAFactura = xmldDoc.SelectSingleNode("//MontoIVAFactura").InnerText;
+                            string montoFactura = xmldDoc.SelectSingleNode("//TotalFactura").InnerText;
+
+                            ListViewItem itemFactura = new ListViewItem(
+                                new[] { nombreCliente, numeroFactura, fechaFactura, montoIVAFactura, montoFactura });
+                            listView.Items.Add(itemFactura);
+                        }
+                    }/*fin foreach*/
+                }
+                else
+                {
+                    MessageBox.Show($"No se encontraron Facturas con la fecha: {fechaSeleccionada} ", "Distribuidora La Zarzuela", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error general al buscar facturas: {ex.Message}", "Distribuidora La Zarzuela", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }/*BuscarFacturas*/
+
+        private void CalcularImpuestoTotal()
+        {
+            decimal impuestoAFavor = 0;
+            decimal impuestoPorPagar = 0;
+
+            // Iterar sobre las facturas de ventas
+            foreach (ListViewItem itemVenta in lvwFacturasVenta.Items)
+            {
+                decimal montoIVAFactura = decimal.Parse(itemVenta.SubItems[3].Text, CultureInfo.InvariantCulture);
+                impuestoAFavor += montoIVAFactura;
+            }
+
+            // Iterar sobre las facturas de compras
+            foreach (ListViewItem itemCompra in lvwFacturasCompra.Items)
+            {
+                decimal montoIVAFactura = decimal.Parse(itemCompra.SubItems[3].Text, CultureInfo.InvariantCulture);
+                impuestoPorPagar += montoIVAFactura;
+            }
+
+            // Calcular el resultado total
+            decimal resultadoTotal = impuestoAFavor - impuestoPorPagar;
+
+            // Mostrar el resultado en el label correspondiente
+            if (resultadoTotal > 0)
+            {
+                lblCalculoRes.Text = $"Impuesto a favor: {resultadoTotal:C}";
+            }
+            else if (resultadoTotal < 0)
+            {
+                lblCalculoRes.Text = $"Impuesto por pagar: {Math.Abs(resultadoTotal):C}";
+            }
+            else
+            {
+                lblCalculoRes.Text = "No hay impuesto a favor ni por pagar.";
+            }
+        } /*CalcularImpuestoTotal*/
 
 
     }/*fin frmCalculoHacienda*/
